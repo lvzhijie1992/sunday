36d4dbe692ec7dddeefe1447b8ae854f40597480
锦色年华 <jinsenianhua@jinsenianhuademacbook-air.local>
2018-06-06 08:50:35 +0800

    哈哈哈哈哈哈哈哈哈

--- sunday/AppDelegate.swift
+++ sunday/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // 我的
+        // 我的啦啦
         return true
     }
 
